0727a756475410c97069d75a6d33ee8036466590
josephhatfield211 <jhjoeman224@gmail.com>
2018-10-25 21:28:28 -0400

    done maybe

--- src/problem2.py
+++ src/problem2.py
@@ -2,10 +2,10 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Joey Hatfield.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import math
 import time
 import testing_helper
 
@@ -90,10 +90,16 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    index = 0
+    top = 0
+    for k in range(len(sequence)):
+        if math.fabs(sequence[k]) > top:
+            index = k
+            top = math.fabs(sequence[k])
+    return index
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
